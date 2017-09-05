23dffe86a91142b8a8cba6fdf1a0177ddd14808b
Connie Wang <conniewang2012@u.northwestern.edu>
2017-09-05 02:34:46 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(n)
-  if n % 3 == 0
+  if n % 3 == 0 && n % 5 == 0
+  "FizzBuzz"
+  elsif n % 3 == 0
     "Fizz"
   elsif n % 5 == 0
     "Buzz"
-  elsif n % 3 == 0 && n % 5 == 0
-    "FizzBuzz"
   else
     nil
   end
